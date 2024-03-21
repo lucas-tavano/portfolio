bcffaa913ef20c99aacdaf6c8ec037a9493fad56
Lucas Tavano <lucastavanopereira@gmail.com>
2024-03-20 23:45:34 -0300

    changes

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Lucas Tavano",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "DevOps Engineer",
+    bio: "Automate!",
+    email: "lucastavanopereira@gmail.com",
+    linkedin: "lucas-tavano",
+    github: "lucas-tavano",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `portfolio`,
+      href: "https://github.com/lucas-tavano/portfolio",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "My Page!",
+    description: "welcome to my page!",
   },
 
   // CONFIG configration (required)
@@ -53,7 +53,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
